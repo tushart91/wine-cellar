16df376fc6478a53d90b6b0fae2852d56cc9a2ba
Tushar Tiwari <tutiwari@paypal.com>
2013-08-27 05:43:59 -0700

    Changed Server Name

--- routes/wines.js
+++ routes/wines.js
@@ -4,7 +4,7 @@ var Server = mongo.Server,
 	Db = mongo.Db,
 	BSON = mongo.BSONPure;
 
-var server = new Server('hyperlvs11.qa.paypal.com', 27017, {auto_reconnect: true});
+var server = new Server('localhost', 27017, {auto_reconnect: true});
 db = new Db('winedb', server);
 
 db.open(function(err, db) {
